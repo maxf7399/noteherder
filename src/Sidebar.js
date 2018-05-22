1c8ebc625e2a4fd9f1633ae8e34d53dca14a27e4
Max Falkenstein <mrfalken@iu.edu>
2018-05-22 11:44:49 -0400

    import images add jsx to side bar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import quill from './quill.svg'
+import newIcon from './new.png'
+import newHover from './new-hover.png'
+
 const Sidebar = () => {
   return (
     <div className='Sidebar'>
-      Sidebar
+      <div className='logo'>
+        <img src={quill} alt='NoteHerder'/>
+      </div>
+
+      <a href='/notes'>
+        <img 
+        src={newHover} 
+        alt='New note' 
+        />
+        <img
+        src={newIcon}
+        alt='New note'
+        />
+      </a>
+      <div class="SignOut">
+            <button>
+              <i class="fa fa-sign-out"></i>
+            </button>
+          </div>
     </div>
 
   )
