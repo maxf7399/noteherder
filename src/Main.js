790a69a7b53eeff31a3b858e64f63b7b67a53552
Max Falkenstein <mrfalken@iu.edu>
2018-05-23 10:42:14 -0400

    Store notes in Main's state

--- src/Main.js
+++ src/Main.js
@@ -4,14 +4,38 @@ import SideBar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
-const Main = () => {
+class Main extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      notes: [
+          {
+            id: 1,
+            title: 'Why I <3 JS',
+            body: 'Because I like code, and JS is code.',
+          },
+          {
+            id: 2,
+            title: 'Thoughts on breakfast',
+            body: 'I love it!',
+          },
+          {
+            id: 3,
+            title: 'Watching the first episode of Black Mirror with your parents',
+            body: 'Don\'t.',
+          },
+      ]
+    }
+  }
+  render() {
     return( 
       <div className="Main" style={style}> 
         <SideBar />
-        <NoteList />
+        <NoteList notes={this.state.notes}/>
         <NoteForm /> 
       </div>
     )
+  }
 }
 
 const style = {
