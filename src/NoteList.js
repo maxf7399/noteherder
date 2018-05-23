2c3b164eb0eff9989f5c05f2ceb5e037cb3be5de
Max Falkenstein <mrfalken@iu.edu>
2018-05-22 22:16:58 -0400

    set up style for notelist

--- src/NoteList.js
+++ src/NoteList.js
@@ -8,5 +8,31 @@ const NoteList = () => {
         </div>
     )
 }
+const styles = {
+    NoteList: {
+      borderLeft: '1px solid #eee',
+      borderRight: '1px solid #eee',
+      width: '30rem'
+    },
+    h3: {
+      color: '#999',
+      textTransform: 'uppercase',
+      fontSize: '2rem',
+      fontFamily: 'Oxygen',
+      fontWeight: '300',
+      letterSpacing: '3px',
+      margin: '20px 2rem'
+    },
+    notes: {
+      borderTop: '1px solid #eee',
+      overflowY: 'scroll',
+      height: 'calc(100vh - 72px)',
+      listStyle: 'none',
+      marginTop: '1em',
+      padding: '0',
+      width: '100%',
+      color: '#999'
+    }
+  };
 
 export default NoteList
