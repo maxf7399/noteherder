a5fe6c4369ec1362482bc6e303bb396982a08730
Max Falkenstein <mrfalken@iu.edu>
2018-05-22 21:40:26 -0400

    fix format on Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,93 +1,113 @@
-import React from 'react'
+import React from 'react';
 
-import quill from './quill.svg'
-import newIcon from './new.png'
-import newHover from './new-hover.png'
+import quill from './quill.svg';
+import newIcon from './new.png';
+import newHover from './new-hover.png';
 
-const Sidebar = () => {
-  return (
-    <div 
-      className='Sidebar' 
-      style={styles.sidebar}
-      >
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    };
+    this.styles = {
+      sidebar: {
+        width: '6rem',
+        backgroundColor: '#f3f3f3',
+        padding: '0.5rem 0',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      },
+      logo: {
+        fontFamily: 'Fauna One',
+        color: '#666',
+        fontSize: '3rem'
+      },
+      logoImg: {
+        width: '3rem',
+        paddingLeft: '0.5rem'
+      },
+      newNote: {
+        marginTop: '2rem',
+        position: 'relative',
+        width: '4rem'
+      },
+      button: {
+        backgroundColor: 'transparent',
+        border: '0',
+        color: '#008bf8',
+        cursor: 'pointer'
+      },
+      aImage: {
+        position: 'absolute',
+        left: '1rem',
+        width: '4rem',
+        transition: 'opacity 0.25s ease-in-out'
+      },
+      SignOut: {
+        position: 'absolute',
+        bottom: '1rem'
+      },
+      SignOutButton: {
+        outline: 'none'
+      },
+      i: {
+        fontSize: '2rem'
+      }
+    };
+  }
 
-      <div 
-        className='logo'
-        style={styles.logo}
-        >
-        <img 
-          src={quill} 
-          alt='NoteHerder'
-          style={styles.logoImg}
-          />
-      </div>
+  toggleButton(value) {
+    console.log('hide');
+    this.setState({
+      hover: value
+    });
+  }
 
-      <a href='/notes'>
-        <img 
-        src={newHover} 
-        alt='New note' 
-        />
+  render() {
+    let button;
+    if (this.state.hover) {
+      button = (
         <img
-        src={newIcon}
-        alt='New note'
+          src={newIcon}
+          alt='New note'
+          style={this.styles.aImage}
+          onMouseEnter={() => this.toggleButton(false)}
         />
-      </a>
-      <div class="SignOut">
-            <button>
-              <i class="fa fa-sign-out"></i>
-            </button>
-          </div>
-    </div>
+      );
+    } else {
+      button = (
+        <img
+          src={newHover}
+          alt='New note'
+          style={this.styles.aImage}
+          onMouseLeave={() => this.toggleButton(true)}
+        />
+      );
+    }
 
-  )
+    return (
+      <div className='Sidebar' style={this.styles.sidebar}>
+        <div className='logo' style={this.styles.logo}>
+          <img src={quill} alt='Noteherder' style={this.styles.logoImg} />
+        </div>
+
+        <a>{button}</a>
+
+        <div className='SignOut' style={this.styles.SignOut}>
+          <button
+            style={{
+              ...this.styles.button,
+              ...this.styles.SignOutButton
+            }}
+          >
+            <i className='fa fa-sign-out' style={this.styles.i} />
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-const styles = {
-  sidebar : {
-    width: '6rem',
-    backgroundColor: '#f3f3f3',
-    padding: '0.5rem 0',
-    felxDirection:'column',
-    allignItems: 'center',
-  },
-  logo: {
-    fontFamily: '"Fauna One"',
-    color: '#666',
-    fontSize: '3rem'
-  },
-  logoImg: {
-    width: '3rem', 
-    paddingLeft: '0.4rem',
-  },
-  aNewNote: {
-    marginTop: '2rem',
-    position: 'relative',
-    width: '4rem',
-  },
-  button: {
-    backgroundColor: '#00ffffff',
-    border: '0',
-    color: '#008BF8',
-    curser: 'pointer',
-  },
-  aImg: {
-    position: 'absolute', 
-    left: '0',
-    width: "100%",
-    transition: 'opacity 0.25s ease-in-out',
-  },
-  aHoverImgOutline: {
-    opacity: '0',
-  },
-  signOut: {
-    position: 'absolute',
-    bottom: '1rem',
-  },
-  signOutButton: {
-    outline: 'none',
-  },
-  signOutButtonIFa: {
-    fontSize: '2rem',
-  },
-  
-}
-export default Sidebar
+
+export default Sidebar;
