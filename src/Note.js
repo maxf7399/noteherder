bd793792d2582f14871ea39b5a5dbe5104a47264
Max Falkenstein <mrfalken@iu.edu>
2018-05-23 10:34:25 -0400

    add Li to change the way it looks in the host

--- src/Note.js
+++ src/Note.js
@@ -1,21 +1,20 @@
 import React from 'react'
 
-
 const Note = (props) => {
-    return (
-        
-        <div className="note">
+  return (
+    <li className="Note">
+      <div className="note">
         <div className="note-title">
-          {  props.note.title }
+          { props.note.title }
         </div>
         <div className="note-body">
-            <p>
-                {props.note.body}
-            </p>
+          <p>
+            { props.note.body }
+          </p>
         </div>
-        </div>
-    
-    )
+      </div>
+    </li>
+  )
 }
 
 export default Note
--- src/NoteList.js
+++ src/NoteList.js
@@ -1,37 +1,36 @@
 import React from 'react'
 
 import './NoteList.css'
-import './NoteForm.css'
 import Note from './Note'
 
 const NoteList = () => {
   const notes = [
-      {
-          id: 1,
-          title: 'Why I <3 Javascript',
-          body: 'Because I like code and JS is code',
+    {
+      id: 1,
+      title: 'Why I <3 JS',
+      body: 'Because I like code, and JS is code.',
+    },
+    {
+      id: 2,
+      title: 'Thoughts on breakfast',
+      body: 'I love it!',
+    },
+    {
+      id: 3,
+      title: 'Watching the first episode of Black Mirror with your parents',
+      body: 'Don\'t.',
+    },
+  ]
 
-      },
-      {
-          id: 2,
-          title: 'Thoughts on breakfast',
-          body: 'I love it!',
-      },
-      {
-          id: 3, 
-          title: 'Watching the first episode of Black mirror with your parents',
-          body: 'Don\'t.',
-      },
-    ]
   return (
     <div className="NoteList">
-          <h3>Notes</h3>
-          <ul id="notes">
-            <a className="active">
-              { notes.map(note=> <Note note={note} />)}
-            </a>
-          </ul>
-        </div>
+      <h3>Notes</h3>
+      <ul id="notes">
+        <a className="active">
+          { notes.map(note => <Note note={note} />)}
+        </a>
+      </ul>
+    </div>
   )
 }
 
